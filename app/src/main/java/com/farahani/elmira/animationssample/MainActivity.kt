17e6cb01aa94e684aae5ed3ba6078761752fd635
Elmira <elmira_frhn@hotmail.com>
2018-11-01 16:30:57 +0330

    Add ConstraintLayout and ConstraintSet animation

--- app/src/main/java/com/farahani/elmira/animationssample/MainActivity.kt
+++ app/src/main/java/com/farahani/elmira/animationssample/MainActivity.kt
@@ -5,14 +5,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.transition.TransitionManager
-import android.widget.ImageView
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.first_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.first_layout)
         addAnimationOperations()
     }
 
@@ -21,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val constraint1= ConstraintSet()
         constraint1.clone(root)
         val constrant2=ConstraintSet()
-        constrant2.clone(this,R.layout.toolbar)
+        constrant2.clone(this,R.layout.second_layout)
 
         icon3.setOnClickListener{
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
